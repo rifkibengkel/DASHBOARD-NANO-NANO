56f737b38683d5307fc438bbc6006eb3aa77b7ca
Rifky <98086503+rifkibengkel@users.noreply.github.com>
2026-03-31 14:23:50 +0700

    Delete pages/api directory

--- pages/api/reqdel/models/_model.ts
+++
@@ -1,6 +0,0 @@
-import { exeQuery } from "../../../../lib/db";
-
-export const checkEntry = (entriesId: string) => {
-    let checkQuery = `SELECT is_valid from entries WHERE UPPER(coupon) LIKE $1 AND (is_valid = 1 OR "invalidReasonId" = 4)`
-    return exeQuery(checkQuery, [`%${entriesId}%`])
-}
